7baaa9e7b80e95fe33d116510976618526123af9
ChaeMoon <jungchaemoon@gmail.com>
2019-07-22 22:10:57 +0900

    Added templates file

--- navi/urls.py
+++ navi/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from navi.views import Navi_view
+from navi.views import *
 
 app_name = 'navi'
 
 urlpatterns = [
-    path('', Navi_view.as_view(), name='home')
-
+    path('', Navi_view.as_view(), name='home'),
+    path('chart/', ChartView.as_view(), name='chart')
 ]
