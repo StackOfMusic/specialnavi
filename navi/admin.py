5fa474a7736589e3257813b4a9a71ec0fa50d94b
ChaeMoon <jungchaemoon@gmail.com>
2019-08-30 22:25:03 +0900

    Updated model

--- navi/admin.py
+++ navi/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from navi.models import *
 # Register your models here.
+
+admin.site.register(Policeoffice)
+admin.site.register(Guns)
+admin.site.register(Guns_detail)
--- navi/models.py
+++ navi/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Policeoffice(models.Model):
+    police_name = models.CharField(max_length=10, blank=True)
+
+    def __str__(self):
+        return self.police_name
+
+
+class Guns(models.Model):
+    police = models.ForeignKey(Policeoffice, on_delete=models.CASCADE)
+    gun_name = models.CharField(max_length=10, blank=True)
+
+    def __str__(self):
+        return self.gun_name
+
+
+class Guns_detail(models.Model):
+    gun = models.ForeignKey(Guns, on_delete=models.CASCADE)
+    gun_type = models.CharField(max_length=5, blank=True)
+    gun_owner = models.CharField(max_length=3, blank=True)
+    gun_age = models.CharField(max_length=4, blank=True)
+
